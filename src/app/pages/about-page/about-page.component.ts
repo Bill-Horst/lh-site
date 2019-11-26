6a3520cecb85e6e1768766f9029308b879b642e1
Bill Horst <billhorst7@gmail.com>
2019-11-25 20:43:02 -0700

    got remaining math tests to support levelling

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -9,16 +9,26 @@ import { StorageService } from 'src/app/services/storage.service';
 export class AboutPageComponent implements OnInit {
 
   public perfectCount: number;
+
   public perfectAdditionCount: number;
-  public perfectSubtractionCount: number;
-  public perfectMultiplicationCount: number;
-  public perfectDivisionCount: number;
-
   public additionTestIndicatorPercentage: number;
-
   public currentAdditionCountByLevel: number;
-
   public currentAdditionLevel: number;
+
+  public perfectSubtractionCount: number;
+  public subtractionTestIndicatorPercentage: number;
+  public currentSubtractionCountByLevel: number;
+  public currentSubtractionLevel: number;
+
+  public perfectMultiplicationCount: number;
+  public multiplicationTestIndicatorPercentage: number;
+  public currentMultiplicationCountByLevel: number;
+  public currentMultiplicationLevel: number;
+
+  public perfectDivisionCount: number;
+  public divisionTestIndicatorPercentage: number;
+  public currentDivisionCountByLevel: number;
+  public currentDivisionLevel: number;
 
   public userLevelSet: any;
 
@@ -27,14 +37,29 @@ export class AboutPageComponent implements OnInit {
   ) { 
     let perfectTests = this.storageService.getPerfectTestCount();
     this.perfectCount = perfectTests.total;
-    this.perfectAdditionCount = perfectTests.addition;
-    this.perfectSubtractionCount = perfectTests.subtraction;
-    this.perfectMultiplicationCount = perfectTests.multiplication;
-    this.perfectDivisionCount = perfectTests.division;
     this.userLevelSet = this.storageService.getUserLevelSet();
+
+    this.perfectAdditionCount = perfectTests.addition;
     this.additionTestIndicatorPercentage = this.calculatePercentage('addition');
     this.currentAdditionCountByLevel = this.userLevelSet.addition.currentScore - this.userLevelSet.addition.previousLevelMarker;
     this.currentAdditionLevel = this.userLevelSet.addition.currentLevel;
+
+    this.perfectSubtractionCount = perfectTests.subtraction;
+    this.subtractionTestIndicatorPercentage = this.calculatePercentage('subtraction');
+    this.currentSubtractionCountByLevel = this.userLevelSet.subtraction.currentScore - this.userLevelSet.subtraction.previousLevelMarker;
+    this.currentSubtractionLevel = this.userLevelSet.subtraction.currentLevel;
+
+    this.perfectMultiplicationCount = perfectTests.multiplication;
+    this.multiplicationTestIndicatorPercentage = this.calculatePercentage('multiplication');
+    this.currentMultiplicationCountByLevel = this.userLevelSet.multiplication.currentScore - this.userLevelSet.multiplication.previousLevelMarker;
+    this.currentMultiplicationLevel = this.userLevelSet.multiplication.currentLevel;
+
+    this.perfectDivisionCount = perfectTests.division;
+    this.divisionTestIndicatorPercentage = this.calculatePercentage('division');
+    this.currentDivisionCountByLevel = this.userLevelSet.division.currentScore - this.userLevelSet.division.previousLevelMarker;
+    this.currentDivisionLevel = this.userLevelSet.division.currentLevel;
+
+    // break out all these properties into one single object and do ngFor in the html
   }
 
   ngOnInit() {
@@ -56,6 +81,21 @@ export class AboutPageComponent implements OnInit {
     if (subject === 'addition') {
       this.userLevelSet;
       let set = this.userLevelSet.addition;
+      return (set.currentScore - set.previousLevelMarker) / (set.currentNextLevel - set.previousLevelMarker) * 100;
+    }
+    if (subject === 'subtraction') {
+      this.userLevelSet;
+      let set = this.userLevelSet.subtraction;
+      return (set.currentScore - set.previousLevelMarker) / (set.currentNextLevel - set.previousLevelMarker) * 100;
+    }
+    if (subject === 'multiplication') {
+      this.userLevelSet;
+      let set = this.userLevelSet.multiplication;
+      return (set.currentScore - set.previousLevelMarker) / (set.currentNextLevel - set.previousLevelMarker) * 100;
+    }
+    if (subject === 'division') {
+      this.userLevelSet;
+      let set = this.userLevelSet.division;
       return (set.currentScore - set.previousLevelMarker) / (set.currentNextLevel - set.previousLevelMarker) * 100;
     }
   }
